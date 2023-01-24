ebb5be46146ea2e197e20c864b60a42d65bf7da1
Anton Polikarpov <a.polikarpov@ficto.ru>
2023-01-24 14:38:25 +0300

    HW 3 is done(may be)

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,56 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type kv struct {
+	Key   string
+	Value int
+}
+
+func Top10(inptStr string) []string {
+	strSlice := strings.Fields(inptStr)
+	resSlice := make([]string, 0, 10)
+	dup := make(map[string]int)
+	var dupKv []kv
+	for _, word := range strSlice {
+		_, exist := dup[word]
+		if exist {
+			dup[word] += 1
+		} else {
+			dup[word] = 1
+		}
+	}
+	for key, val := range dup {
+		dupKv = append(dupKv, kv{key, val})
+	}
+	sort.Slice(dupKv, func(i, j int) bool {
+		return dupKv[i].Value > dupKv[j].Value
+	})
+	if len(dupKv) >= 10 {
+		topKv := dupKv[0:10]
+		var lastVal int
+		var curNumCounterPos int
+		for idx, kvTemp := range topKv {
+			if lastVal != kvTemp.Value {
+				sliceKvSort(topKv[curNumCounterPos:idx])
+				curNumCounterPos = idx
+			} else if lastVal == kvTemp.Value && idx == len(topKv)-1 {
+				sliceKvSort(topKv[curNumCounterPos:])
+			}
+			lastVal = kvTemp.Value
+		}
+		for _, kvt := range topKv {
+			resSlice = append(resSlice, kvt.Key)
+		}
+	}
+	return resSlice
+}
+
+func sliceKvSort(slice []kv) {
+	sort.Slice(slice, func(i, j int) bool {
+		return slice[i].Key < slice[j].Key
+	})
 }
