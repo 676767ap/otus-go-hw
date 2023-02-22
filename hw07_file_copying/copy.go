c7d0f25d41ab95f9aac275964dce8245f302f433
Anton Polikarpov <a.polikarpov@ficto.ru>
2023-02-22 12:15:49 +0300

    HW7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -10,6 +15,49 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var l int64
+
+	inFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	inF, err := inFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset > inF.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	outFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = inFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 || limit > inF.Size() {
+		l = inF.Size()
+	} else {
+		l = limit
+	}
+
+	bar := pb.New(int(l)).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 1)
+	bar.ShowSpeed = true
+	bar.Start()
+	reader := bar.NewProxyReader(inFile)
+
+	if _, err := io.CopyN(outFile, reader, l); err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
+	bar.Finish()
 	return nil
 }
