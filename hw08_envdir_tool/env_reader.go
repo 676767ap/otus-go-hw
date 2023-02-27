1b778430bf7bc760d92d9136861f8ab9b6efce9c
Anton Polikarpov <a.polikarpov@ficto.ru>
2023-02-27 17:31:38 +0300

    HW8

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,64 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
 	Value      string
 	NeedRemove bool
 }
 
-// ReadDir reads a specified directory and returns map of env variables.
-// Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	envs := make(Environment)
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		envLine, err := prepareLine(filepath.Join(dir, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		if len(envLine) == 0 {
+			envs[file.Name()] = EnvValue{"", true}
+		} else {
+			envs[file.Name()] = EnvValue{envLine, false}
+		}
+	}
+	return envs, nil
+}
+
+func prepareLine(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewReader(file)
+	line, _, err := scanner.ReadLine()
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return "", nil
+		}
+		return "", err
+	}
+
+	line = bytes.ReplaceAll(line, []byte("\x00"), []byte("\n"))
+	trimmedLine := strings.TrimRight(string(line), " ")
+	return trimmedLine, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,41 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env.
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+)
+
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint
+	command.Stdout = os.Stdout
+
+	for k, v := range env {
+		_, ok := os.LookupEnv(k)
+		if v.NeedRemove {
+			if ok {
+				os.Unsetenv(k)
+			}
+			continue
+		}
+		os.Setenv(k, v.Value)
+	}
+	command.Env = os.Environ()
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Start(); err != nil {
+		log.Fatalf("Start command: %v", err)
+	}
+
+	if err := command.Wait(); err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		log.Fatalf("Wait run: %v", err)
+	}
 	return
 }
